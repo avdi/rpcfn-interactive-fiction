f490a6a39a69b2602d3fab20702f8f138bc6cff0
Avdi Grimm <pair+avdi@pivotallabs.com>
2010-04-11 23:47:01 -0400

    Clarified some points in README. Expanded skeleton play.rb. Added more synonyms to petite_cave.if.

--- bin/play.rb
+++ bin/play.rb
@@ -1,7 +1,27 @@
 #!/usr/bin/env ruby
 
 class Game
-  # TODO implement me!
+  def initialize(story_path, options={})
+    @input  = options.fetch(:input)  { $stdin  }
+    @output = options.fetch(:output) { $stdout }
+  end
+
+  def play!
+    start!
+    execute_one_command! until ended?
+  end
+
+  def start!
+    raise NotImplementedError, "Implement me!"
+  end
+
+  def execute_one_command!
+    raise NotImplementedError, "Implement me!"
+  end
+
+  def ended?
+    raise NotImplementedError, "Implement me!"
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
